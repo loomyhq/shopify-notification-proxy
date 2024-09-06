de4f68863cb43b54d9adde581311db4990782a2a
Christophe De Troyer <christophe.detroyer@gmail.com>
2024-09-06 13:50:39 +0200

    fix gettext

--- lib/notification_proxy_web/gettext.ex
+++ lib/notification_proxy_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule NotificationProxyWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :notification_proxy
+  use Gettext.Backend, otp_app: :notification_proxy
 end
