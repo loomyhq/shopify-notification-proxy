147ad17d014f05f58fbe58b7cec699752d001744
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-09-23 08:42:10 +0200

    add discount codes to message

--- lib/notification_proxy_web/controllers/shopify_controller.ex
+++ lib/notification_proxy_web/controllers/shopify_controller.ex
@@ -6,9 +6,18 @@ defmodule NotificationProxyWeb.ShopifyController do
     total = get_in(params, ["current_subtotal_price_set", "shop_money", "amount"])
     firstname = get_in(params, ["customer", "first_name"])
     lastname = get_in(params, ["customer", "last_name"])
+    discount_codes = get_in(params, ["discount_codes"])
+
+    message = "#{firstname} #{lastname} heeft een bestelling geplaatst voor €#{total} 🥳"
+
+    discounts =
+      case discount_codes do
+        [] -> ""
+        dcs -> " Kortingscodes: #{Enum.join(dcs, ", ")}"
+      end
 
     params = %{
-      text: "#{firstname} #{lastname} heeft een bestelling geplaatst voor €#{total} 🥳"
+      text: message <> discounts
     }
 
     HTTPoison.post(url, Jason.encode!(params), [{"Content-Type", "application/json"}])
